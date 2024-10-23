99d8abc0f45f57c0a49fbb9905533aa9fa52c3bb
prime0x2 <prime0x2@gmail.com>
2024-10-23 13:15:11 +0600

    added custom expiry in generate token function

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,10 +1,10 @@
-import jwt, { JwtPayload, TokenExpiredError } from "jsonwebtoken";
+import jwt, { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
 
-import ENV from "./env";
-import AppError from "../utils/appError";
+import ENV from './env';
+import AppError from '../utils/appError';
 
-export const generateToken = (payload: JwtPayload) => {
-  return jwt.sign(payload, ENV.JWT_SECRET, { expiresIn: ENV.JWT_EXPIRY });
+export const generateToken = (payload: JwtPayload, expiresIn: string = ENV.JWT_EXPIRY) => {
+  return jwt.sign(payload, ENV.JWT_SECRET, { expiresIn });
 };
 
 export const verifyToken = (token: string) => {
@@ -12,9 +12,9 @@ export const verifyToken = (token: string) => {
     return jwt.verify(token, ENV.JWT_SECRET);
   } catch (error) {
     if (error instanceof TokenExpiredError) {
-      throw new AppError(401, "Token expired. Please login again.");
+      throw new AppError(401, 'This token has expired.');
     } else {
-      throw new AppError(401, "Invalid token. Please login again.");
+      throw new AppError(401, 'This token is invalid.');
     }
   }
 };
