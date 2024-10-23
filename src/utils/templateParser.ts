2560da755f27761fd7c52d741e154bdc28ccab76
prime0x2 <prime0x2@gmail.com>
2024-10-23 13:18:19 +0600

    added email template + template parsing function

---
+++ src/utils/templateParser.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import Handlebars from 'handlebars';
+
+// templates/activationMail.html
+
+export interface ActivationTemplateData {
+  firstName: string;
+  activationLink: string;
+}
+
+const templateParser = async (data: ActivationTemplateData) => {
+  try {
+    const source = fs.readFileSync(__dirname + '/templates/activationMail.html', 'utf8');
+    const sourceCode = Handlebars.compile(source);
+    const template = sourceCode(data);
+
+    console.log('✅ Template parsed successfully');
+
+    return template;
+  } catch (error) {
+    console.log('❌ Error parsing template: ', error);
+    throw error;
+  }
+};
+
+export default templateParser;
