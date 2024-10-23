042554c70c1a841cc1aa70bf5ee454a008b6db56
prime0x2 <prime0x2@gmail.com>
2024-10-23 13:18:48 +0600

    removed unnecessary use of "any" type

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 const catchAsync = (fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch((err: any) => next(err));
+    fn(req, res, next).catch((err) => next(err));
   };
 };
 
